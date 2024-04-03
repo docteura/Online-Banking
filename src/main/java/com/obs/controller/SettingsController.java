90f6527c499a875e8f07ca3cee5fe74d15b6bc2a
Babu Devandla <34365254+babudevandla@users.noreply.github.com>
2024-04-03 15:04:16 +0800

    settings added
    settings added

---
+++ src/main/java/com/obs/controller/SettingsController.java
@@ -0,0 +1,32 @@
+package com.obs.controller;
+
+import java.security.Principal;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.obs.domain.User;
+import com.obs.service.LoansService;
+import com.obs.service.UserService;
+
+@Controller
+@RequestMapping("/settings")
+public class SettingsController {
+
+	@Autowired
+	private UserService userService;
+
+	@Autowired
+	LoansService loansService;
+
+	@GetMapping("/details")
+	public String settings(Principal principal, Model model) {
+		User user = userService.findByUsername(principal.getName());
+
+		model.addAttribute("user", user);
+		return "settings";
+	}
+}
